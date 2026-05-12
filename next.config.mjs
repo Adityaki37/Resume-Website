75bdc04fd690ea04d39f9ff39bbb8d8d96786d43
Adityaki37 <122066012+Adityaki37@users.noreply.github.com>
2026-05-12 19:27:47 -0400

    Add Fireboy route redirect config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/fireboy',
+        destination: 'https://www.youtube.com/watch?v=to107gIfwyg',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
